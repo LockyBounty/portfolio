be5ba08a356ebab8579d932054de2034c67dbc52
Loki <bounthienglocky@gmail.com>
2020-01-21 06:15:22 +0100

    fixed - gap on mobile version by reducing some element sizes

--- locky.js
+++ locky.js
@@ -1,1 +1,34 @@
-console.log('hello');
+const URL1 = "https://www.google.com/"
+
+// document.getElementById('test1').addEventListener('mouseover',function(){
+//         alert("hello!")
+        
+//      }
+//     );
+document.getElementById('test1').addEventListener('click',function(){
+//    alert("hello!")
+window.open(URL1, '_blank');
+ }
+);
+
+// document.querySelector('.content2').addEventListener('click',function(){
+//     console.log("hello!")
+//  }
+//  );
+
+ document.querySelector('.slider-txt').addEventListener('click',function(){
+    console.log("hello!")
+ }
+ );
+
+//  $( '.modal' ).addClass( 'open' );
+
+//  if ( $( '.modal' ).hasClass( 'open' ) ) {
+//    $( '.container' ).addClass( 'blur' );
+//  } 
+
+//  $( '.close' ).click(function() {
+//     $( '.modal' ).removeClass( 'open' );
+//     $( '.cont' ).removeClass( 'blur' );
+//   });
+

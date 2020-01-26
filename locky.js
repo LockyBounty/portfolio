650c187fd99755ccf081f3858ce3bacb53fbf749
Loki <bounthienglocky@gmail.com>
2020-01-26 03:35:56 +0100

    added - 4 project pages

--- locky.js
+++ locky.js
@@ -1,62 +1,68 @@
-const URL1 = "projet1.html"
+const URL1 = "projet1.html";
+const URL2 = "projet2.html";
+const URL3 = "projet3.html";
+const URL4 = "projet4.html";
+const URL5 = "projet5.html";
+const URL6 = "projet6.html";
+const URL7 = "projet7.html";
+const URL8 = "projet8.html";
 
-let menu = document.querySelector('.nav-toggle');
 
-document.querySelector('#nav-check1').addEventListener('click',function(){
-   menu.checked = false
+let menu = document.querySelector(".nav-toggle");
+
+document.querySelector("#nav-check1").addEventListener("click", function() {
+  menu.checked = false;
 });
-document.querySelector('#nav-check2').addEventListener('click',function(){
-   menu.checked = false
+document.querySelector("#nav-check2").addEventListener("click", function() {
+  menu.checked = false;
 });
-document.querySelector('#nav-check3').addEventListener('click',function(){
-   menu.checked = false
+document.querySelector("#nav-check3").addEventListener("click", function() {
+  menu.checked = false;
 });
-document.querySelector('#nav-check4').addEventListener('click',function(){
-   menu.checked = false
+document.querySelector("#nav-check4").addEventListener("click", function() {
+  menu.checked = false;
 });
 
+// document.getElementById('projet1').addEventListener('mouseover',function(){
+//         console.log("hello!")
 
+//      }
+//     );
 
+document.getElementById("projet1").addEventListener("click", function() {
+  //    alert("hello!")
+  // window.open(URL1, '_blank');
+  window.open(URL1, "_self");
+});
+document.getElementById("projet2").addEventListener("click", function() {
+  window.open(URL2, "_self");
+});
 
+document.getElementById("projet3").addEventListener("click", function() {
+  window.open(URL3, "_self");
+});
 
+document.getElementById("projet4").addEventListener("click", function() {
+  window.open(URL4, "_self");
+});
 
-document.getElementById('projet1').addEventListener('mouseover',function(){
-        console.log("hello!")
-        
-     }
-    );
-document.getElementById('projet1').addEventListener('click',function(){
-//    alert("hello!")
-// window.open(URL1, '_blank');
-window.open(URL1, '_self');
- }
-);
+document.getElementById("projet5").addEventListener("click", function() {
+  window.open(URL5, "_self");
+});
+
+document.getElementById("projet6").addEventListener("click", function() {
+  window.open(URL6, "_self");
+});
+
+document.getElementById("projet7").addEventListener("click", function() {
+  window.open(URL7, "_self");
+});
+
 
 // document.querySelector('.content2').addEventListener('click',function(){
 //     console.log("hello!")
 //  }
 //  );
 
- document.querySelector('.slider-txt').addEventListener('click',function(){
-    console.log("hello!")
- }
- );
 
-
-
-
-
-// $('#manual-ajax').click(function(event) {
-//    event.preventDefault();
-//    this.blur(); // Manually remove focus from clicked link.
-//    $.get(this.href, function(html) {
-//      $(html).appendTo('body').modal();
-//    });
-//  });
-
-//  $("#manual-ajax").modal({
-//    escapeClose: false,
-//    clickClose: false,
-//    showClose: false
-//  });
 
--- modal.js
+++ modal.js
@@ -1,8 +1,21 @@
  var url0 = 'index.html';
 
 
- document.querySelector(".modal-close-button2", ".modal-close-up").addEventListener('click',function(){
+ document.querySelector(".modal-close-button2").addEventListener('click',function(){
    //    alert("hello!")
    window.open(url0, "_self");
     }
    );
+   document.querySelector(".modal-close-button1").addEventListener('click',function(){
+    //    alert("hello!")
+    window.open(url0, "_self");
+     }
+    );
+
+
+
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
+
